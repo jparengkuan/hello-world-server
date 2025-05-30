75c2e59f68a6f024fda02ed80645b2f93320d57f
Jim Parengkuan <jim.parengkuan@gmail.com>
2025-05-30 17:44:49 +0200

    Updated CI

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
     res.end(JSON.stringify({ message: 'Hello World!' }));
 });
 
-// Listen on port 80
-server.listen(8080, () => {
+// Listen on port 3000
+server.listen(3000, () => {
     console.log('Server running at http://localhost:80/');
 });
