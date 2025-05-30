06c3eebe4998cfdba74bfb882267d8551a506862
Jim Parengkuan <jim.parengkuan@gmail.com>
2025-05-31 00:48:23 +0200

    Updated CI

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // Set response headers
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    if (req.url === '/health' && req.method === 'GET') {
+        const randomValue = Math.random();
 
-    // Send JSON response
+        // Simulate a 50% chance of returning an error response
+        if (randomValue < 0.5) {
+            res.writeHead(500, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ status: 'error', message: 'Internal Server Error' }));
+            return;
+        }
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ status: 'ok' }));
+        return;
+    }
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Hello World!' }));
 });
 
 // Listen on port 3000
 server.listen(3000, () => {
-    console.log('Server running at http://localhost:80/');
+    console.log('Server running at http://localhost:3000/');
 });
